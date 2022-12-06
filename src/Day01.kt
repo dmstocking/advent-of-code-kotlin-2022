60db879b2f91294ad69094049d0931919b267ac7
David Stocking <david.stocking@stryker.com>
2022-12-06 10:19:35 -0500

    task: Implemented Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val grouped = mutableListOf<MutableList<Int>>()
+        grouped.add(mutableListOf())
+        input.forEach {
+            if (it.isBlank()) {
+                grouped.add(mutableListOf())
+            } else {
+                grouped[grouped.size-1].add(it.toInt())
+            }
+        }
+        return grouped.maxOf { it.sum() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val grouped = mutableListOf<MutableList<Int>>()
+        grouped.add(mutableListOf())
+        input.forEach {
+            if (it.isBlank()) {
+                grouped.add(mutableListOf())
+            } else {
+                grouped[grouped.size-1].add(it.toInt())
+            }
+        }
+        return grouped.map { it.sum() }.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
